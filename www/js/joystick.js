37985d36d6568779e24ce8f30af303711ff6408a
Jakob Ferdinandsen <jakob@jhaaf.dk>
2017-03-26 19:30:55 +0200

    Init commit

---
+++ www/js/joystick.js
@@ -0,0 +1,47 @@
+var radius = 100;
+
+var rightNipple = {
+    zone: document.getElementById("rightZone"),
+    mode: 'static',
+    position: {
+      left: '80%',
+      top: '70%'
+    },
+    size: radius*2,
+    color: 'black'
+};
+var leftNipple = {
+    zone: document.getElementById("leftZone"),
+    mode: 'static',
+    position: {
+      left: '20%',
+      top: '70%'
+    },
+    size: radius*2,
+    color: 'black'
+};
+
+
+var leftJoystick;
+var leftPosition;
+var rightJoystick;
+var rightPosition;
+
+leftJoystick = nipplejs.create(leftNipple);
+leftJoystick.on('start end', function(evt, data) {
+  leftPosition.distance = 0;
+}).on('move', function(evt, data) {
+  leftPosition = data;
+});
+
+rightJoystick = nipplejs.create(rightNipple);
+rightJoystick.on('start end', function(evt, data) {
+  rightPosition.distance = 0;
+}).on('move', function(evt, data) {
+  rightPosition = data;
+});
+$(function(){
+  setInterval(function(){
+    app.sendData(leftPosition, rightPosition);
+  }, 100);
+});
